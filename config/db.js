e51b6dabb3cb594b195a7608bef159ca9cb22dab
Prashant Sharma <prashantsharma7470@gmail.com>
2024-08-03 23:51:17 +0530

    Your commit message

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(`mongodb://localhost:27017/Razorpayment`)
         console.log('Database Connected'.rainbow);
 
     } catch (error) {
